aa695fbd9c34009f5f151494b18558541f409182
Pete Lewis <121941598+FenderBender2@users.noreply.github.com>
2026-08-01 08:00:46 +0100

    Add files via upload

--- TSW-Save-File-Manager-Source/My Project/Resources.Designer.vb
+++ TSW-Save-File-Manager-Source/My Project/Resources.Designer.vb
@@ -39,7 +39,7 @@ Namespace My.Resources
         Friend ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
             Get
                 If Object.ReferenceEquals(resourceMan, Nothing) Then
-                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("TSW_Save_Game_Manager.Resources", GetType(Resources).Assembly)
+                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("TSW_Save_File_Manager.Resources", GetType(Resources).Assembly)
                     resourceMan = temp
                 End If
                 Return resourceMan
