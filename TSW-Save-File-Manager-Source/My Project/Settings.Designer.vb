e4405c832682026c56bbd82682fd9af479362309
Pete Lewis <121941598+FenderBender2@users.noreply.github.com>
2026-08-18 22:15:56 +0100

    Add files via upload

--- TSW-Save-File-Manager-Source/My Project/Settings.Designer.vb
+++ TSW-Save-File-Manager-Source/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "18.8.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "18.9.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -99,6 +99,18 @@ Namespace My
             End Get
             Set
                 Me("lastTabOrder") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property lastProfile() As String
+            Get
+                Return CType(Me("lastProfile"),String)
+            End Get
+            Set
+                Me("lastProfile") = value
             End Set
         End Property
     End Class
